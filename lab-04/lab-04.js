516be29a4b9e427acc5927cbe5916823340e3ee5
NakishaLin <105456796+NakishaLin@users.noreply.github.com>
2022-06-03 13:10:20 -0700

    Add files via upload

---
+++ lab-04/lab-04.js
@@ -0,0 +1,44 @@
+/*
+    CIT 281 Lab 4
+    Name: Nakisha Lin 
+*/
+
+const { request } = require("http");
+
+// Require the Fastify framework and instantiate it
+const fastify = require("fastify")();
+// Handle GET verb for / route using Fastify
+// Note use of "chain" dot notation syntax
+fastify.get("/", function (request, reply) {
+  reply
+    .code(200)
+    .header("Content-Type", "text/html; charset=utf-8")
+    .send("<h1>Hello from Lab 4!</h1>");
+});
+//name 
+fastify.get("/name", function(request, reply) {
+    const { first, last} = request.query;
+    if ( first && last){
+    reply
+      .code(200)
+      .header("Content-Type", "text/html; charset=utf-8")
+      .send(`<h1>Hello ${first}, ${last} </h1>`);
+  } else{
+    reply
+      .code(200)
+      .header("Content-Type", "text/html; charset=utf-8")
+      .send('<h1>Hello Guest!</h1>');
+  }
+
+});
+// Start server and listen to requests using Fastify
+const listenIP = "localhost";
+const listenPort = 8080;
+fastify.listen(listenPort, listenIP, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${address}`);
+});
+
